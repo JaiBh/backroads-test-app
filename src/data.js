b88aa51231692c2c682c5c81cd28c27dfcc9fe4c
Jai Bhullar <81278507+JaiBh@users.noreply.github.com>
2024-04-14 01:03:28 +0100

    first commit

---
+++ src/data.js
@@ -0,0 +1,115 @@
+import tour1 from "./images/tour-1.jpeg";
+import tour2 from "./images/tour-2.jpeg";
+import tour3 from "./images/tour-3.jpeg";
+import tour4 from "./images/tour-4.jpeg";
+import tour5 from "./images/tour-5.jpeg";
+import tour6 from "./images/tour-6.jpeg";
+
+export const pageLinks = [
+  { id: 1, href: "#home", text: "home" },
+  { id: 2, href: "#about", text: "about" },
+  { id: 3, href: "#services", text: "services" },
+  { id: 4, href: "#tours", text: "tours" },
+];
+
+export const socialLinks = [
+  {
+    id: 1,
+    href: "https://www.twitter.com",
+    icon: "fab fa-twitter",
+  },
+  {
+    id: 2,
+    href: "https://www.facebook.com",
+    icon: "fab fa-facebook",
+  },
+  {
+    id: 3,
+    href: "https://www.twitter.com",
+    icon: "fab fa-squarespace",
+  },
+];
+
+export const services = [
+  {
+    id: 1,
+    icon: "fas fa-wallet fa-fw",
+    title: "saving money",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Asperiores, officia.",
+  },
+  {
+    id: 2,
+    icon: "fas fa-wallet fa-fw",
+    title: "saving money",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Asperiores, officia.",
+  },
+  {
+    id: 3,
+    icon: "fas fa-wallet fa-fw",
+    title: "saving money",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Asperiores, officia.",
+  },
+];
+
+export const tours = [
+  {
+    id: 1,
+    img: tour1,
+    title: "Tibet Adventure",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    date: "August 26th, 2020",
+    days: 20,
+    country: "Kenya",
+    price: 3300,
+  },
+  {
+    id: 2,
+    img: tour2,
+    title: "Tibet Adventure",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    date: "August 26th, 2020",
+    days: 20,
+    country: "Kenya",
+    price: 3300,
+  },
+  {
+    id: 3,
+    img: tour3,
+    title: "Tibet Adventure",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    date: "August 26th, 2020",
+    days: 20,
+    country: "Kenya",
+    price: 3300,
+  },
+  {
+    id: 4,
+    img: tour4,
+    title: "Tibet Adventure",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    date: "August 26th, 2020",
+    days: 20,
+    country: "Kenya",
+    price: 3300,
+  },
+  {
+    id: 5,
+    img: tour5,
+    title: "Tibet Adventure",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    date: "August 26th, 2020",
+    days: 20,
+    country: "Kenya",
+    price: 3300,
+  },
+  {
+    id: 6,
+    img: tour6,
+    title: "Tibet Adventure",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    date: "August 26th, 2020",
+    days: 20,
+    country: "Kenya",
+    price: 3300,
+  },
+];
